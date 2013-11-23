2fc71e6b38bbc4915ef60a83e7dae48b971bdaca
HARIHARAN <hariharanwebmail@gmail.com>
2013-11-23 19:43:26 +0530

    Move over keys sequentially

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -7,7 +7,7 @@ var musicControllers = angular.module('musicControllers', []);
 musicControllers.controller('musicController', ['$scope','$timeout', function($scope,$timeout){
   
 	var whiteKeys = ["C1","D1","E1","F1","G1","A1","B1","C2","D2","E2","F2","G2","A2","B2","C3"];
-	var blackKeys = ["C#1","D#1","F#1","G#1","Bb1","C#2","D#2","F#2","G#2","Bb2"];
+	var blackKeys = ["Csharp1","Dsharp1","Fsharp1","Gsharp1","Bb1","Csharp2","Dsharp2","Fsharp2","Gsharp2","Bb2"];
 	$scope.keys = _.union(whiteKeys,blackKeys);
 	
 	var currentIndex = 0;
@@ -20,9 +20,21 @@ musicControllers.controller('musicController', ['$scope','$timeout', function($s
 	var pressKeys = function(){
 		if(currentIndex == $scope.keys.length) 
 			currentIndex = 0;
-		console.log("Press key "+$scope.keys[currentIndex++]);
-		keyPressingKeys();
+			
+			console.log($scope.keys[currentIndex]);
+			
+			_.each($('.black'), function(key){
+				key.style.fill = "black";
+			});
+			_.each($('.white'), function(key){
+				key.style.fill = "white";
+			});			
+			$('.'+$scope.keys[currentIndex])[0].style.fill = "yellow";
+			currentIndex++;
+		 	keyPressingKeys();
 	}
+	
+
 	
 	keyPressingKeys();
 }]);
